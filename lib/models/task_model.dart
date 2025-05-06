be893cfcce87633c24e1ef2c25a10866db500418
Bright <137806076+Finest321@users.noreply.github.com>
2025-05-06 21:05:10 +0100

    Update task_model.dart

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,15 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Task {
   final String id;
   final String title;
   final bool isDone;
+  final DateTime createdAt;
 
-  Task({required this.id, required this.title, this.isDone = false});
+  Task({
+    required this.id,
+    required this.title,
+    required this.createdAt,
+    this.isDone = false,
+  });
 
   factory Task.fromMap(Map<String, dynamic> map, String id) {
     return Task(
       id: id,
       title: map['title'],
       isDone: map['isDone'] ?? false,
+      createdAt: (map['createdAt'] as Timestamp).toDate(),
     );
   }
 
@@ -17,14 +26,22 @@ class Task {
     return {
       'title': title,
       'isDone': isDone,
+      'createdAt': Timestamp.fromDate(createdAt),
     };
   }
 
-  Task copyWith({String? id, String? title, bool? isDone}) {
+  Task copyWith({
+    String? id,
+    String? title,
+    bool? isDone,
+    DateTime? createdAt,
+  }) {
     return Task(
       id: id ?? this.id,
       title: title ?? this.title,
       isDone: isDone ?? this.isDone,
+      createdAt: createdAt ?? this.createdAt,
     );
   }
 }
+
